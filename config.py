57464c7ed328adc7df1b332ebc449dcf837fc2de
XonLoke <fengshui0011@gmail.com>
2024-12-09 16:42:27 +0800

    ver1.0

---
+++ config.py
@@ -0,0 +1,6 @@
+
+class Config:
+    # MySQL Database Configuration
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:12345678@localhost/task_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SECRET_KEY = 'secret_key'
